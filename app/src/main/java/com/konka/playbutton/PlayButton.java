7a0b919d9dbde0899e63db905fccac6c08c9e57b
wangjiert <wangjiert@gmail.com>
2016-09-26 17:36:57 +0800

    改进圆弧缩短的实现

--- app/src/main/java/com/konka/playbutton/PlayButton.java
+++ app/src/main/java/com/konka/playbutton/PlayButton.java
@@ -1,5 +1,7 @@
 package com.konka.playbutton;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
@@ -8,6 +10,8 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -18,6 +22,7 @@ public class PlayButton extends Button {
     AnimatorSet animatorSet = new AnimatorSet();
     Path path = new Path();
     Paint paint = new Paint();
+    Paint cleanPaint = new Paint();
     RectF rectF;
     float lengthValue = 0.05f;
     float rotationValue = 0f;
@@ -37,6 +42,8 @@ public class PlayButton extends Button {
     }
 
     private void init(AttributeSet attrs, int defStyle) {
+        //cleanPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        cleanPaint.setColor(getDrawingCacheBackgroundColor());
         rectF = new RectF(100, 100, 600 - 100, 600 - 100);
         paint.setColor(Color.BLUE);
         paint.setAntiAlias(true);
@@ -50,6 +57,13 @@ public class PlayButton extends Button {
         rotationAnimator.setDuration(4000);
 
         animatorSet.play(lengthAnimator).before(rotationAnimator);
+        animatorSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                animatorSet.start();
+            }
+        });
         animatorSet.start();
     }
 
@@ -59,8 +73,8 @@ public class PlayButton extends Button {
     }
 
     private void setRotationValue(float rotationValue){
-        if(rotationValue > 300f) {
-            lengthValue = (360f - rotationValue) > 0.05 ? (360f - rotationValue) : 0.05f;
+        if(rotationValue > 180) {
+            lengthValue = (360f - rotationValue) / 3 > 0.05 ? (360f - rotationValue) / 3 : 0.05f;
         }
         this.rotationValue = rotationValue;
         invalidate();
@@ -72,7 +86,7 @@ public class PlayButton extends Button {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        Log.i("Test", lengthValue + "length");
+        canvas.drawPaint(cleanPaint);
         int saveCount = canvas.save();
         canvas.rotate(rotationValue, 300f, 300f);
         path.addArc(rectF, -30f, lengthValue);
