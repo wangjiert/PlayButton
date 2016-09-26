28415bcedeb642ba45228d7b37a7cd157fec793b
wangjiert <wangjiert@gmail.com>
2016-09-26 16:53:50 +0800

    当旋转结束时缩短圆弧的长度

--- app/src/main/java/com/konka/playbutton/MainActivity.java
+++ app/src/main/java/com/konka/playbutton/MainActivity.java
@@ -2,12 +2,19 @@ package com.konka.playbutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    PlayButton playButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playButton = (PlayButton) findViewById(R.id.button);
     }
+
+    public void click(View view) {
+        playButton.startAnimator();
+    }
+
 }
--- app/src/main/java/com/konka/playbutton/PlayButton.java
+++ app/src/main/java/com/konka/playbutton/PlayButton.java
@@ -2,6 +2,7 @@ package com.konka.playbutton;
 
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -14,6 +15,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class PlayButton extends Button {
+    AnimatorSet animatorSet = new AnimatorSet();
     Path path = new Path();
     Paint paint = new Paint();
     RectF rectF;
@@ -46,7 +48,7 @@ public class PlayButton extends Button {
         lengthAnimator.setDuration(4000);
         ObjectAnimator rotationAnimator = ObjectAnimator.ofFloat(this, "rotationValue", 360f);
         rotationAnimator.setDuration(4000);
-        AnimatorSet animatorSet = new AnimatorSet();
+
         animatorSet.play(lengthAnimator).before(rotationAnimator);
         animatorSet.start();
     }
@@ -57,18 +59,27 @@ public class PlayButton extends Button {
     }
 
     private void setRotationValue(float rotationValue){
+        if(rotationValue > 300f) {
+            lengthValue = (360f - rotationValue) > 0.05 ? (360f - rotationValue) : 0.05f;
+        }
         this.rotationValue = rotationValue;
         invalidate();
     }
 
+    public void startAnimator(){
+        animatorSet.start();
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
+        Log.i("Test", lengthValue + "length");
         int saveCount = canvas.save();
         canvas.rotate(rotationValue, 300f, 300f);
         path.addArc(rectF, -30f, lengthValue);
         path.addArc(rectF, 90f, lengthValue);
         path.addArc(rectF, 210f, lengthValue);
         canvas.drawPath(path, paint);
+        path.reset();
         super.onDraw(canvas);
         canvas.restoreToCount(saveCount);
     }
